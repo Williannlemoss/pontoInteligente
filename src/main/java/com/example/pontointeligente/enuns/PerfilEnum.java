65bb966ba0b5f7282fa5157f588936337f00be0b
João Celedõnio <joao.celedonio@lead.fortaleza>
2021-07-27 08:16:56 -0300

    Organização do back

---
+++ src/main/java/com/example/pontointeligente/enuns/PerfilEnum.java
@@ -0,0 +1,6 @@
+package com.example.pontointeligente.enuns;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+}
---
+++ src/test/java/com/example/pontointeligente/repositories/LancamentoRepositoryTest.java
@@ -0,0 +1,100 @@
+package com.example.pontointeligente.repositories;
+
+import static org.junit.Assert.assertEquals;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+import java.util.List;
+
+import com.example.pontointeligente.enuns.PerfilEnum;
+import com.example.pontointeligente.enuns.TipoEnum;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.*;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.pontointeligente.entities.Empresa;
+import com.example.pontointeligente.entities.Funcionario;
+import com.example.pontointeligente.entities.Lancamento;
+import com.example.pontointeligente.utils.PasswordUtils;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class LancamentoRepositoryTest {
+	
+	@Autowired
+	private LancamentoRepository lancamentoRepository;
+	
+	@Autowired
+	private FuncionarioRepository funcionarioRepository;
+	
+	@Autowired
+	private EmpresaRepository empresaRepository;
+	
+	private Long funcionarioId;
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		Empresa empresa = this.empresaRepository.save(obterDadosEmpresa());
+		
+		Funcionario funcionario = this.funcionarioRepository.save(obterDadosFuncionario(empresa));
+		this.funcionarioId = funcionario.getId();
+		
+		this.lancamentoRepository.save(obterDadosLancamentos(funcionario));
+		this.lancamentoRepository.save(obterDadosLancamentos(funcionario));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		this.empresaRepository.deleteAll();
+	}
+
+	@Test
+	public void testBuscarLancamentosPorFuncionarioId() {
+		List<Lancamento> lancamentos = this.lancamentoRepository.findByFuncionarioId(funcionarioId);
+		
+		assertEquals(2, lancamentos.size());
+	}
+	
+//	@Test
+//	public void testBuscarLancamentosPorFuncionarioIdPaginado() {
+//		PageRequest page = new PageRequest.of(1, 10);
+//		Page<Lancamento> lancamentos = this.lancamentoRepository.findByFuncionarioId(funcionarioId, page);
+//
+//		assertEquals(2, lancamentos.getTotalElements());
+//	}
+	
+	private Lancamento obterDadosLancamentos(Funcionario funcionario) {
+		Lancamento lancameto = new Lancamento();
+		lancameto.setData(new Date());
+		lancameto.setTipo(TipoEnum.INICIO_ALMOCO);
+		lancameto.setFuncionario(funcionario);
+		return lancameto;
+	}
+
+	private Funcionario obterDadosFuncionario(Empresa empresa) throws NoSuchAlgorithmException {
+		Funcionario funcionario = new Funcionario();
+		funcionario.setNome("Fulano de Tal");
+		funcionario.setPerfil(PerfilEnum.ROLE_USUARIO);
+		funcionario.setSenha(PasswordUtils.gerarBCrypt("123456"));
+		funcionario.setCpf("24291173474");
+		funcionario.setEmail("email@email.com");
+		funcionario.setEmpresa(empresa);
+		return funcionario;
+	}
+
+	private Empresa obterDadosEmpresa() {
+		Empresa empresa = new Empresa();
+		empresa.setRazaoSocial("Empresa de exemplo");
+		empresa.setCnpj("51463645000100");
+		return empresa;
+	}
+
+}
